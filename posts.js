233445f88898b9cd6ee22fdc0d13e40095ebbf54
Gianluigi Santoro <gianluigi.santoro@wolterskluwer.com>
2026-04-20 10:26:48 +0200

    post2 fix

--- posts.js
+++ posts.js
@@ -10,7 +10,7 @@ const POSTS = [
     body: `<p>Inizio senza sapere dove arrivo. Non è un blog nel senso classico. Non è un diario. È qualcosa di mezzo.</p>
 <p>Un posto dove buttare giù quello che ho in testa, senza filtri, senza algoritmi che decidono cosa vale la pena dire.</p>
 <p>Forse non lo leggerà nessuno. Va bene così.</p>`
-  }
+  },
 {
     id: 2,
     date: "2026-04-20",
